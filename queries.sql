ccc292b3553044974f5f5869a5b87817b7a33289
krismuch <49840263+krismuch@users.noreply.github.com>
2019-07-30 17:20:11 -0400

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -2,7 +2,8 @@
 SELECT e.emp_no, e.first_name, e.last_name, e.gender, s.salary 
 FROM employees AS e 
 INNER JOIN salaries AS s 
-ON e.emp_no=s.emp_no;
+ON e.emp_no=s.emp_no
+ORDER BY e.emp_no;
 
 --List employees who were hired in 1986.
 SELECT emp_no, first_name, last_name, hire_date 
@@ -24,7 +25,8 @@ FROM employees AS e
 INNER JOIN dept_emp AS de 
 ON e.emp_no=de.emp_no 
 INNER JOIN departments AS d 
-ON de.dept_no=d.dept_no;
+ON de.dept_no=d.dept_no
+ORDER BY e.emp_no;
 
 --List all employees whose first name is "Hercules" and last names begin with "B."
 SELECT emp_no, first_name, last_name 
